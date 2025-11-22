497a1ed05358a9fdfd3252084d39c7a77451efc5
EliseoDavid <eliseodv10@gmail.com>
2025-11-22 18:27:05 -0500

    Fix encoding config.js

--- frontend/client/config.js
+++ frontend/client/config.js
@@ -1,5 +1,3 @@
-// Configuración del API Gateway
-// Cambia esta URL por la URL de tu API Gateway después del deployment
 const API_CONFIG = {
     baseURL: 'https://c9sut9oprg.execute-api.us-east-1.amazonaws.com',
     tenantId: 'pardos-chicken'
